2da2c06d8f5752d20bd0268bc02041fa4253d65e
SerafimNascimento <serafim.snj@gmail.com>
2025-10-22 20:16:03 -0700

    Initial commit

---
+++ src/test/java/serafim/mavenproject/SimpleTests.java
@@ -0,0 +1,12 @@
+package serafim.mavenproject;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTests {
+    @Test
+    public void verifyMath() {
+        int result = 2 + 2;
+        Assert.assertEquals(result, 4, "Math check");
+    }
+}
